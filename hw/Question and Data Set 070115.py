30c6eff94da9c40069214251233c560a8f9f1be7
Angela Truong <angela.truong@ymail.com>
2015-07-01 02:12:56 -0700

    Question and Data set

---
+++ hw/Question and Data Set 070115.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+July 1 (Mandatory): Question and Data Set 
+
+
+What is the question you hope to answer? What data are you planning to use to answer that question? What do you know about the data so far? Why did you choose this topic?
+Example:
+
+I'm planning to predict passenger survival on the Titanic.
+I have Kaggle's Titanic dataset with 10 passenger characteristics.
+I know that many of the fields have missing values, that some of the text fields are messy and will require cleaning, and that about 38% of the passengers in the training set survive.
+I chose this topic because I'm fascinated by the history of the Titanic.
+"""
+
+'''
+I’m trying to find out where I can find the best cheap tacos in the bay area.
+
+I plan to scrape data from yelp.
+
+Yelp is a pretty useful tool with it’s filter functions but maybe we can be even more specific:
+Only consider restaurants with four and up star rating.
+Users leave comments regarding how delicious food is at the restaurant which I will have to clean and exclude every food except tacos. Keyword filtering.
+Have to clean out the users who are not trustworthy.
+Maybe single out users who frequent taco restaurants often.
+View only restaurants with one ‘$’ symbol.
+Set a minimum number to reviews requirement for restaurants.
+Only consider authentic tacos. (No fusion)
+Restaurants only. (No food trucks)
+
+I chose this topic because I frequent different areas in the bay and I tend to crave for tacos when someone asks what I would like to eat for lunch/dinner. This topic will be useful to everybody.
+'''
